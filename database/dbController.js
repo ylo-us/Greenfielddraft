9ead79ee18d7e3b4dbcb8261a07438de02fa7cb8
Gary  Taylor <gtt@Garys-MBP.home>
2016-09-08 09:24:58 -0400

    Download first two thousand listings from database rather than first 1100 so we have to go to API calls less

--- database/dbController.js
+++ database/dbController.js
@@ -5,55 +5,74 @@ var CLmodel = require('./config.js');
 var CL = require('./Craigslist.js');
 var neighbourhoodCodes = require('./nhcodes.js')
 var Promise = require('../node_modules/bluebird')
+var request = require('../node_modules/request')
+var cheerio = require('../node_modules/cheerio')
+request = Promise.promisify(request)
 // CL.getLinkswithSigWord('sfbay','roo',0).then(function(data){
 // 	var record = new CLmodel({cityandsearchkey:'sfbayroo',listings:JSON.stringify(data)})
 // 	record.save();
 // 	console.log('saved')
 // })
 
-//DOWNLOADS FIRST 1100 LISTINGS FROM GIVEN CITY
-//NEED TO UPDATE SO IT GETS ALL LISTINGS BASED ON THE TOTALCOUNT PROPERTY IN CRAIGSLIST
+//DOWNLOADS FIRST 2000 LISTINGS FROM A GIVEN CITY, OR ALL LISTINGS, WHICHEVER IS LARGER
 
 function downloadAllfromCity(city){
 	return new Promise(function(resolve,reject){
+		var url = 'http://' + city + '.craigslist.org/search/' + "roo" + '?s=' + "0";
 
-		var tasks = [];
+		request(url).then(function(req){
+			//console.log('REQ',req.body);
+			$ = cheerio.load(req.body);
+			var listingcount = Number($('.totalcount').html())
+			if(listingcount>2000){listingcount = 2000;}
+		
 
-		for(var i =0;i<1100; i+=100){
-			tasks.push(CL.getLinkswithSigWord.bind(null,city,'roo',i));
-		}
-		console.log(tasks.length);
 
-		// tasks[1]().then(function(data){
-		// 	console.log(data);
-		// })
+			var tasks = [];
 
-		function Download(){
-			var listings = [];
-			var internal = function(j){
-				console.log('Finished ',j)
-				tasks[j]().then(function(data){
-					listings = listings.concat(data);
-					if(j<tasks.length-1){
-						internal(j+1);
-					}
-					if(j === tasks.length-1){
-						resolve(listings);
-					}
-				})
+			for(var i =0;i<listingcount; i+=100){
+				tasks.push(CL.getLinkswithSigWord.bind(null,city,'roo',i));
+			}
+			console.log(tasks.length);
+
+			// tasks[1]().then(function(data){
+			// 	console.log(data);
+			// })
+
+			function Download(){
+				var listings = [];
+				var internal = function(j){
+					console.log('Finished ',j)
+					tasks[j]().then(function(data){
+						listings = listings.concat(data);
+						if(j<tasks.length-1){
+							internal(j+1);
+						}
+						if(j === tasks.length-1){
+							resolve(listings);
+						}
+					})
+				}
+
+				internal(0);
 			}
 
-			internal(0);
-		}
-
-		Download()
+			Download()
+		})
 	})
 }
 
 module.exports.saveCityData = function(city){
 	downloadAllfromCity(city).then(function(data){
-		var record = new CLmodel({cityandsearchkey:city,listings:JSON.stringify(data)});
-		record.save();
+		var data = JSON.stringify(data);
+		var record = new CLmodel({cityandsearchkey:city,listings:data});
+		console.log('DATA LENGTH',data.length)
+		record.save(function(err){
+			if(err){console.log(err)} else {
+				console.log('Saved!')
+			}
+		})
+		console.log('saved');
 	})
 }
 
@@ -132,6 +151,4 @@ function filterByPriceandNeighborhood(data,nh,nhs,maxprice){
 	})
 }
 
-// module.exports.getdatafromDBbyUrl('http://sfbay.craigslist.org/search/sfc/roo?nh=22').then(function(data){
-// 	console.log(data);
-// })
+// module.exports.saveCityData('sfbay')
