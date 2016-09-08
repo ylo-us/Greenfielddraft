221110a15c2db090b482e8d41365c0a6c70210ad
Gary  Taylor <gtt@Garys-MBP.home>
2016-09-08 09:31:33 -0400

    Return only first five listings from each neighborhood

--- APIs/APIcall.js
+++ APIs/APIcall.js
@@ -2,13 +2,34 @@ var Yelp = require('./yelp-results.js')
 var CL = require('./searchcraigslist');
 var Promise = require('../node_modules/bluebird');
 
+var sortListings = function(data) {
+	var finalData = {};
+	finalData.neighbourhoods = data.neighbourhoods;
+	finalData.listings = {};
+
+	for(var neighbourhood in data.listings) {
+	  finalData.listings[neighbourhood] = [];
+	  var i=0;
+	  var counter=0
+
+	  while(counter < 5 && i < data.listings[neighbourhood].length) {
+	        if (data.listings[neighbourhood][i].lat !== undefined) {
+	      finalData.listings[neighbourhood].push(data.listings[neighbourhood][i]);
+	      counter+=1
+	      }
+	    i+=1;
+	  }
+	}
+	return finalData;
+};
+
 //currently only works
 module.exports.getNeighborhoodsandListings = function(city,terms){
   return new Promise(function(resolve,reject){
     Yelp.getRecommendedNeighborhoods(city,terms).then(function(data){
       data = data.map(function(obj){return obj.neighborhood});
       CL.returnCraigsListlistingsByNeighborhood(data).then(function(data){
-        resolve(data);
+        resolve(sortListings(data));
       })
     })
   })
